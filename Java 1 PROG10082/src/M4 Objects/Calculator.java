7e59ea65e242fe212fef4f231e30fa6e25240408
Anne <steamengine.a@gmail.com>
2023-02-16 16:12:31 -0500

    Create Calculator.java

---
+++ Java 1 PROG10082/src/M4 Objects/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+     public double answer;
+ 
+     public void add (double number){
+         answer += number;
+     }
+ 
+     public void subtract (double number){
+         this.answer = this.answer - number;
+     }
+ 
+     public void multiply (double number){
+         this.answer = this.answer * number;
+     }
+ 
+     public void divide(double number){
+         answer /= number;
+     }
+ 
+     public void clear(){
+         answer = 0;
+     }
+ 
+     public double getAnswer(){
+         return this.answer;
+     }
+ 
+ }
