1a702e238be78def4ee35a8ddbdc66b8a21ede2d
Anne <steamengine.a@gmail.com>
2023-02-16 18:35:50 -0500

    Create GuessTheNumber.java

---
+++ Java 1 PROG10082/src/M5 If/GuessTheNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+    public static void main(String[] args){
+
+        Guess g1 = new Guess();
+        g1.numberGenerator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess the number between 1 - 10");
+        String result = g1.makeGuess(scanner.nextInt());
+        System.out.println(result);
+    }
+}
