116464293bb229489701d77114b5af73c5a364b4
Anne <steamengine.a@gmail.com>
2023-02-16 20:44:51 -0500

    Create TestGrade.java

---
+++ Java 1 PROG10082/src/M4 Objects/TestGrade.java
@@ -0,0 +1,15 @@
+public class TestGrade { 
+
+	public static void main(String[] args){ 
+
+		Grade student1 = new Grade(); 
+		student1.name = "Anne"; 
+		student1.java1 = 95; 
+		student1.dataNetworking = 86; 
+		student1.math = 83; 
+		student1.webDevelopment = 70; 
+		
+		System.out.println(student1.getAve()); 
+		System.out.println(student1.transcript()); 
+	} 
+}
