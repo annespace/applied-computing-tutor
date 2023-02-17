81b53e479fb755395c55e57fc40c2f2f8df1df71
Anne <steamengine.a@gmail.com>
2023-02-16 19:44:24 -0500

    Create MethodOverloading.java

---
+++ Java 1 PROG10082/src/M6 GettersSetters - Constructors - Overloaded Methods/MethodOverloading.java
@@ -0,0 +1,10 @@
+public class MethodOverloading
+{
+    public static void main(String[] args) {
+        Overloading ave = new Overloading();
+        System.out.println(ave.getAve(5));
+        System.out.println(ave.getAve(5,7));
+        System.out.println(ave.getAve(5,7,9));
+        System.out.println(ave.getAve(5.0,7,9));
+    }
+}
