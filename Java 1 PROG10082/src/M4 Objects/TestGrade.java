0e0c0cfbc974b5b6d1f263c09f3c9dacb72c0856
Anne <steamengine.a@gmail.com>
2023-02-18 15:48:37 -0500

    Update TestGrade.java

--- Java 1 PROG10082/src/M4 Objects/TestGrade.java
+++ Java 1 PROG10082/src/M4 Objects/TestGrade.java
@@ -1,13 +1,31 @@
+/** 
+* 
+* @author Anne Cho
+*/
+
+import java.util.Scanner;
+
 public class TestGrade { 
 
 	public static void main(String[] args){ 
 
+		// Explain the difference between Scanner class and Grade class?
+		Scanner input = new Scanner(System.in);
 		Grade student1 = new Grade(); 
+
 		student1.name = "Anne"; 
-		student1.java1 = 95; 
-		student1.dataNetworking = 86; 
-		student1.math = 83; 
-		student1.webDevelopment = 70; 
+
+		System.out.println("Enter the java1 marks: ");
+		student1.java1 = input.nextInt(); 
+
+		System.out.println("Enter the data networking marks: ");
+		student1.dataNetworking = input.nextInt(); 
+
+		System.out.println("Enter the math marks: ");
+		student1.math = input.nextInt(); 
+
+		System.out.println("Enter the web development marks: ");
+		student1.webDevelopment = input.nextInt(); 
 		
 		System.out.println(student1.getAve()); 
 		System.out.println(student1.transcript()); 
