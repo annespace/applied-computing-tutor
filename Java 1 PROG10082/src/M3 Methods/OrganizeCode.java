e06f9105d7b08e4ad67c39c4f91178e361cc4287
Anne <steamengine.a@gmail.com>
2023-02-18 15:28:36 -0500

    Create OrganizeCode.java

---
+++ Java 1 PROG10082/src/M3 Methods/OrganizeCode.java
@@ -0,0 +1,49 @@
+/** 
+* 
+* @author Anne Cho 
+*/
+
+public class OrganizeCode {
+
+     public static void main (String[] args) { 
+		
+		System.out.println("Let's Organize Your Time With Methods!");
+	
+		morning(); 
+		afternoon(); 
+		evening(); 
+		night(); 
+	}
+	 
+	public static void morning() { 
+	
+		System.out.println("Wear a nice dress"); 
+		System.out.println("Wear a nice perfume as well!"); 
+		System.out.println("Leave the house, get to work"); 
+	
+	} 
+	
+	public static void afternoon() { 
+	
+		System.out.println("Work, Work, Work");
+		System.out.println("Go get a chicken sandwich");
+		System.out.println("Go get some coffee!");
+	} 
+	
+	public static void evening() { 
+	
+		System.out.println("Get home");
+		System.out.println("Make a plan for a trip");
+		System.out.println("Do yoga");
+		System.out.println("Take a shower");
+
+	} 
+	
+	public static void night() { 
+	
+		System.out.println("Sleep tight! 🌝"); 
+
+	} 
+
+     
+}
