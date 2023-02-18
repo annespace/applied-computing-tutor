b9f38b15b2a2e3846b8a5779d274b1514a52034a
Anne <steamengine.a@gmail.com>
2023-02-18 14:13:05 -0500

    Update TaxCalculator.java

--- Java 1 PROG10082/src/M2 Variables/TaxCalculator.java
+++ Java 1 PROG10082/src/M2 Variables/TaxCalculator.java
@@ -1,3 +1,8 @@
+/** 
+* 
+* @author Anne Cho 
+*/
+
 import java.util.Scanner; 
 
 public class TaxCalculator { 
@@ -8,8 +13,10 @@ public class TaxCalculator {
 		final double ONTARIO_TAX_RATE = 1.13; 
 		
 		Scanner input = new Scanner(System.in); 
+
 		System.out.println("Before tax price: "); 
 		beforeTaxPrice = input.nextDouble(); 
+		
 		double afterTaxPrice = beforeTaxPrice * ONTARIO_TAX_RATE; 
 		System.out.println("After tax price: " + afterTaxPrice); 
 	} 
