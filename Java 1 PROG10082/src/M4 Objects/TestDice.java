9fd94988f3108bb5f8bf3253131f87ca680c4e3d
Anne <steamengine.a@gmail.com>
2023-02-18 15:48:33 -0500

    Create TestDice.java

---
+++ Java 1 PROG10082/src/M4 Objects/TestDice.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * References for In-Class Exercise 1
+ */
+
+public class TestDice { 
+
+	public static void main (String[] args) { 
+
+		// Creating an object
+		Dice dice = new Dice(); 
+		
+		int sum = 0; 
+		for(int i = 0 ; i < 20 ; i++) { 
+			int number = dice.roll(); 
+			System.out.print(number + "+ "); 
+			sum += number; 
+		} 
+		
+		System.out.println(sum); 
+		
+		} 
+}
