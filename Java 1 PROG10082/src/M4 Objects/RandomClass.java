073db4c59aafdcfcc59e3f8321aaa22eb18565af
Anne <steamengine.a@gmail.com>
2023-02-18 15:50:22 -0500

    Update RandomClass.java

--- Java 1 PROG10082/src/M4 Objects/RandomClass.java
+++ Java 1 PROG10082/src/M4 Objects/RandomClass.java
@@ -1,3 +1,8 @@
+/** 
+* 
+* @author Anne Cho
+*/
+
 import java.util.Random; 
 
 public class RandomClass {
@@ -9,19 +14,20 @@ public class RandomClass {
 
 	public static void main(String[] args){ 
 	
-		int a = 5; 
-		Random rnd = new Random(); 
-		//rnd = local variable. it’s declared inside of a method
-		//rnd = reference variable. type Random starts with a capital
+		double anne = 5; 
+		Random randomAnne = new Random(); 
+		//anne = local variable. it’s declared inside of a method
+		//randomAnne = reference variable. type Random starts with a capital
 /* 
 Random is built into Java. somebody wrote the class called Random.
 if you want to create your own type in Java, ***create a class.***
+
 The Random class file is in the Java API which is stored on the hard drive.
 System.out.println is also in the Java API. 
 */
-		int number = rnd.nextInt(100); 
+		int randomnumber = randomAnne.nextInt(100); 
 //Method called nextInt(), in the Random class, some smart person wrote.
-		System.out.println(number); 
+		System.out.println("The random number is " + randomnumber); 
 	}
 
 }
