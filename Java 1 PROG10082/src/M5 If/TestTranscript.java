b3c958784b9f8bae1d4f25521e6d4c351e256830
Anne <steamengine.a@gmail.com>
2023-02-18 23:03:30 -0500

    Create TestTranscript.java

---
+++ Java 1 PROG10082/src/M5 If/TestTranscript.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TestTranscript {
+
+     public static void main(String[] args) {
+
+          Scanner input = new Scanner(System.in);
+          Transcript anne = new Transcript();
+
+          System.out.print("Enter the marks you've got: ");
+          System.out.println(anne.calculate(input.nextDouble()));
+     }
+     
+}
