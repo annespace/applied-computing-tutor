9d3b2805c3afaa2a1f352b8b802011da154c032f
Anne <steamengine.a@gmail.com>
2023-02-19 14:40:27 -0500

    Create TestNationalMuseumOfKorea.java

---
+++ Java 1 PROG10082/src/M6 GettersSetters - Constructors - Overloaded Methods/TestNationalMuseumOfKorea.java
@@ -0,0 +1,26 @@
+/** 
+* 
+* @author Anne Cho
+*/
+
+public class TestNationalMuseumOfKorea {
+
+     public static void main(String[] args) throws Exception {
+
+     NationalMuseumOfKorea ticket1 = new NationalMuseumOfKorea();
+     ticket1.setName("Anne");
+     ticket1.setQty(2);
+     ticket1.setMonth(2);
+     ticket1.setDay(19);
+     System.out.println(ticket1.getName());
+     System.out.println(ticket1.notification());
+
+     NationalMuseumOfKorea ticket2 = new NationalMuseumOfKorea("Molly", 3, 3, 11);
+     System.out.println(ticket2.getName());
+     System.out.println(ticket2.getQty());
+     System.out.println(ticket2.notification());
+
+     NationalMuseumOfKorea ticket3 = new NationalMuseumOfKorea("Lia", 1000, 5, 27);
+     System.out.println(ticket3.notification());
+     }
+}
