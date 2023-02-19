26c0274a6961b22c87c8e962a8946d2228eac3c8
Anne <steamengine.a@gmail.com>
2023-02-18 23:03:33 -0500

    Create Transcript.java

---
+++ Java 1 PROG10082/src/M5 If/Transcript.java
@@ -0,0 +1,20 @@
+public class Transcript {
+
+     private double studentAverage = 75.8;
+
+     public String calculate(double marks){
+
+          String text ="";
+          if (marks - studentAverage >= 10) {
+               text += "You've got high marks!";
+          }
+          else if (Math.abs(marks-studentAverage) < 10) {
+               text += "You're okay!";
+          }
+          else {
+               text += "Don't give up, keep going!";
+          }
+          return text;
+     }
+     
+}
