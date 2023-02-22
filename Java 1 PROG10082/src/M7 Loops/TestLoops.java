7e64569ed0798b217c70c974e3c55cfd22947704
Anne <steamengine.a@gmail.com>
2023-02-22 16:40:52 -0500

    Create TestLoops.java

---
+++ Java 1 PROG10082/src/M7 Loops/TestLoops.java
@@ -0,0 +1,11 @@
+public class TestLoops {
+     public static void main(String[] args) {
+
+          Loops loop1 = new Loops();
+          loop1.whileLoop(3);
+          loop1.forLoop(3);
+          loop1.doWhileLoop(3);
+          
+     }
+     
+}
