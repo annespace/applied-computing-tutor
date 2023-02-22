30dbddc120c7f709e49b0f8cc68cf97fef92e784
Anne <steamengine.a@gmail.com>
2023-02-22 16:40:54 -0500

    Create Loops.java

---
+++ Java 1 PROG10082/src/M7 Loops/Loops.java
@@ -0,0 +1,31 @@
+public class Loops {
+
+     public void whileLoop(int to){
+          System.out.println("WHILE LOOP");
+          int from = 0;
+          while(from<=to){
+               System.out.print(from + ", ");
+               from++;
+          }         
+     }
+
+     public void forLoop(int to){
+          System.out.println("\nFOR LOOP");
+          for(int from = 0; from <=to; from++){
+               System.out.print(from + ", ");
+          }
+     }
+
+     public void doWhileLoop (int to){
+          System.out.println("\nDO WHILE LOOP");
+          int from = 0;
+          do {
+          System.out.print(from + ", ");
+          from++;
+          }
+          while (from <= to);
+     }
+
+
+}
+     
