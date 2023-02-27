4bf7e75d32360e1ae95f13678f96770aee390467
Anne <steamengine.a@gmail.com>
2023-02-27 11:43:05 -0500

    Create SimpleObject.java

---
+++ Java 1 PROG10082/src/M8 Arrays/SimpleObject.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * References for Assignment
+ */
+
+public class SimpleObject {
+
+     private int instVar;
+
+     public void setInstVar(int instVar){
+          this.instVar = instVar;
+     }
+
+     public int getInstVar(){
+          return instVar;
+     }
+  
+}
