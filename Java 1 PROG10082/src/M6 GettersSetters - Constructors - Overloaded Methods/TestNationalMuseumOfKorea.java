f78b2050da60b4650f90564b7d12f9f3926c7af7
Anne <steamengine.a@gmail.com>
2023-02-27 11:49:04 -0500

    Update TestNationalMuseumOfKorea.java

--- Java 1 PROG10082/src/M6 GettersSetters - Constructors - Overloaded Methods/TestNationalMuseumOfKorea.java
+++ Java 1 PROG10082/src/M6 GettersSetters - Constructors - Overloaded Methods/TestNationalMuseumOfKorea.java
@@ -13,14 +13,15 @@ public class TestNationalMuseumOfKorea {
      ticket1.setMonth(2);
      ticket1.setDay(19);
      System.out.println(ticket1.getName());
-     System.out.println(ticket1.notification());
+     System.out.println(ticket1);
 
      NationalMuseumOfKorea ticket2 = new NationalMuseumOfKorea("Molly", 3, 3, 11);
      System.out.println(ticket2.getName());
-     System.out.println(ticket2.getQty());
-     System.out.println(ticket2.notification());
+     System.out.println(ticket2);
 
      NationalMuseumOfKorea ticket3 = new NationalMuseumOfKorea("Lia", 1000, 5, 27);
-     System.out.println(ticket3.notification());
+     System.out.println(ticket3);
      }
+
+     //https://www.javatpoint.com/understanding-toString()-method
 }
