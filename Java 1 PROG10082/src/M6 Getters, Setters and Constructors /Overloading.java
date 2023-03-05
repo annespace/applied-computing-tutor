5b0c29a97eb8bc8f41c2a1ad2de10396f1e38c1c
Anne <steamengine.a@gmail.com>
2023-03-05 16:29:02 -0500

    Create Overloading.java

---
+++ Java 1 PROG10082/src/M6 Getters, Setters and Constructors /Overloading.java
@@ -0,0 +1,34 @@
+/** 
+* 
+* @author Anne Cho
+*/
+
+public class Overloading {
+
+     public int getAve(int n1)
+     {
+         return n1;
+     }
+ 
+     public int getAve(int n1, int n2)
+     {
+         return (n1+n2)/2;
+     }
+ 
+     public double getAve(int n1, int n2, int n3)
+     {
+         return (n1+n2+n3)/3;
+     }
+ 
+     // public int getAve(int n1, int n2, int n3)
+     // {
+     //     return (n1+n2+n3)/3; //error
+     // }
+ 
+     public double getAve(double n1, int n2, int n3)
+     {
+         return (n1+n2+n3)/3;
+     }
+ 
+ }
+ //https://www.javatpoint.com/method-overloading-in-java
