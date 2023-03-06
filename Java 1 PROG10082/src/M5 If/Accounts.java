94c9edd576a803dade822d2e53f13a7adc872f18
Anne <steamengine.a@gmail.com>
2023-03-06 10:11:07 -0500

    Create Accounts.java

---
+++ Java 1 PROG10082/src/M5 If/Accounts.java
@@ -0,0 +1,44 @@
+/*
+ * 
+ * References for Assignment 3 (Class: 1231_18296)
+ */
+
+import java.util.Scanner;
+import static java.lang.System.exit;
+
+public class Accounts {
+
+        public static void main(String[]args)
+        {
+            final double checkingAccount = 15.00;
+            final double savingsAccount = 25.00;
+
+            Scanner input = new Scanner(System.in);
+            System.out.printf("%-34s","Enter the account type: ");
+            int account = input.nextInt();
+            System.out.printf("%-34s","Enter the initial balance: ");
+            double balance = input.nextDouble();
+            double rate = 0.0;
+
+            if (account == 2){
+                rate  = savingsAccount;
+            }
+            else if (account == 1){
+                rate  = checkingAccount;
+            }
+            else {
+                System.out.println("Error! Invalid account type. Must be 1, 2 or 3");
+                exit(0);
+            }
+
+            double NewBalance = balance + rate;
+
+            System.out.printf("===================================\n");
+            System.out.printf("Init. balance " + " Change " + " New Balance \n");
+            System.out.printf("===================================\n");
+            System.out.printf("%.2f \t%.2f\t %.2f\n", balance, rate, NewBalance);
+
+        }
+    }
+
+
