e7fada1fbfe6613b6a591a6aa5c5f25a87345312
Anne <steamengine.a@gmail.com>
2023-03-06 15:46:50 -0500

    Create TestAdvancedTravelHistory.java

---
+++ Java 1 PROG10082/src/M8 Arrays/AdvancedTravelHistory/TestAdvancedTravelHistory.java
@@ -0,0 +1,25 @@
+/** 
+* 
+* @author Anne Cho
+*/
+
+public class TestAdvancedTravelHistory{
+
+     public static void main(String[] args) throws Exception {
+
+          AdvancedTravelHistory h1 = new AdvancedTravelHistory("Anne","London","England");
+          h1.addSpot("Soho");
+          h1.addSpot("Tower Bridge");
+          h1.addSpot("Westminster Abbey");
+          h1.addSpot("Imperial War Museum");
+          h1.printHistory();
+
+          AdvancedTravelHistory h2 = new AdvancedTravelHistory("Min-woo","London","England");
+          h2.addSpot("Soho");
+          h2.addSpot("Tower Bridge");
+          h2.addSpot("Westminster Abbey");
+          h2.addSpot("Imperial War Museum");
+          // h2.addSpot("Shoryu Ramen Liverpool Street");
+          h2.printHistory();
+     }
+}
