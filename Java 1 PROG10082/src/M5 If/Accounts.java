1003b4c2a525fe3cd899f8b9d368c34ca3e1bfba
Anne <steamengine.a@gmail.com>
2023-03-08 12:00:55 -0500

    Update Accounts.java

--- Java 1 PROG10082/src/M5 If/Accounts.java
+++ Java 1 PROG10082/src/M5 If/Accounts.java
@@ -1,44 +1,54 @@
 /*
  * 
  * References for Assignment 3 (Class: 1231_18296)
+ * @Jean-Luc
  */
 
-import java.util.Scanner;
+ import java.util.Scanner;
 import static java.lang.System.exit;
+public class Accounts{
+    public static final double CHANGE_ONE = 0.015;
+    public static final double CHANGE_TWO = 0.025;
+        public static void main(String[]args){
 
-public class Accounts {
-
-        public static void main(String[]args)
-        {
-            final double checkingAccount = 0.015; //1.5%
-            final double savingsAccount = 0.025; //2.5%
-
-            Scanner input = new Scanner(System.in);
-            System.out.printf("%-34s","Enter the account type: ");
-            int account = input.nextInt();
+        Scanner input = new Scanner(System.in);
+        double rate = 0;
+        double newBalance = 0;
+        double shares = 0;
+        double balance = 0;
+        System.out.printf("%-34s","Enter the account type: ");
+        int account = input.nextInt();
+            if(account == 3 || account == 2 || account == 1){
             System.out.printf("%-34s","Enter the initial balance: ");
-            double balance = input.nextDouble();
-            double rate = 0.0;
-
-            if (account == 2){
-                rate  = savingsAccount;
+            balance = input.nextDouble();
+            if (account == 3){
+            System.out.printf("%-34s","Enter initial stock price:");
+            double stock = input.nextDouble();
+            System.out.printf("%-34s","Enter actual stock price:");
+            double actual = input.nextDouble();
+            shares = balance / stock;
+            newBalance = shares * actual;
+            rate = newBalance - balance;
+            }
+            else if (account == 2){
+            rate = balance * CHANGE_TWO;
+            newBalance = balance + rate;
             }
             else if (account == 1){
-                rate  = checkingAccount;
+            rate = balance * CHANGE_ONE;
+            newBalance = balance + rate;
             }
             else {
-                System.out.println("Error! Invalid account type. Must be 1, 2 or 3");
-                exit(0);
             }
-
-            double newBalance = balance + balance * rate;
-
-            System.out.printf("===================================\n");
-            System.out.printf("Init. balance " + " Change " + " New Balance \n");
-            System.out.printf("===================================\n");
-            System.out.printf("%.2f \t%.2f\t %.2f\n", balance, newBalance-balance, newBalance);
-
+            }
+        else {
+        System.out.println("Error! Invalid account type. Must be 1, 2 or 3");
+        System.exit(0);
         }
+    
+    System.out.printf("===================================\n");
+    System.out.printf("Init. balance " + " Change " + " New Balance \n");
+    System.out.printf("===================================\n");
+    System.out.printf("\t%.2f \t %.2f \t %.2f\n", balance, rate, newBalance);
     }
-
-
+}
