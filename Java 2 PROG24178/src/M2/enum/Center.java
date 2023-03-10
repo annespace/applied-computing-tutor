a00d9d65d6b4e599aae0e1bc3becef2e3ead45d4
Anne <steamengine.a@gmail.com>
2023-03-09 20:27:34 -0500

    add Center class

---
+++ Java 2 PROG24178/src/M2/enum/Center.java
@@ -0,0 +1,17 @@
+public class Center {
+     
+     private Ballet ballet;
+
+     public Center(Ballet ballet){
+          this.ballet = ballet;
+     }
+
+     public Ballet getBallet(){
+          return ballet;
+     }
+
+     public void setBallet(Ballet ballet){
+          this.ballet = ballet;
+     }
+     
+}
--- Java 2 PROG24178/src/M2/enum/TestBallet.java
+++ Java 2 PROG24178/src/M2/enum/TestBallet.java
@@ -6,12 +6,20 @@
 public class TestBallet {
 
      public static void main(String[] args) {
-          
+          /* FOR ENUM
           // Ballet anne = new Ballet("advanced");
           // Ballet jennie = new Ballet("advancedd");
           Ballet anne = new Ballet(BalletLevel.ADVANCED);
           Ballet jennie = new Ballet(BalletLevel.INTERMEDIATE);
-          System.out.println(anne.equals(jennie));          
+          System.out.println(anne.equals(jennie)); 
+          */
+
+          Ballet anne = new Ballet(BalletLevel.ADVANCED);
+          Center seoulBranch = new Center(anne);
+          System.out.println(seoulBranch);
+          System.out.println(seoulBranch.getBallet());
+          System.out.println(seoulBranch.getBallet().getBalletLevel());
+          
      }
      
 }
