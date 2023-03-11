abdd108004e93aca9b9756034d62442c9ce5e9d8
Anne <steamengine.a@gmail.com>
2023-03-11 12:31:20 -0500

    add LearnStack classes

---
+++ Java 2 PROG24178/src/M2/StackStructure/Clothing.java
@@ -0,0 +1,24 @@
+/** 
+* 
+* @author Anne Cho
+*/
+
+public class Clothing {
+
+     private String type;
+     private String brand;
+
+     public Clothing(String type, String brand){
+          this.type = type;
+          this.brand = brand;
+     }
+
+     public String getType(){
+          return type;
+     }
+
+     public String getBrand(){
+          return brand;
+     }
+     
+}
